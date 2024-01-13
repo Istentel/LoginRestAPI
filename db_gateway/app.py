ddec1fb1108a440cddee8f68a88c5c1d502b1134
Cristian Alexandru <cristi23pas@yahoo.com>
2024-01-13 13:14:22 +0200

    chore: register and login fully working

--- db_gateway/app.py
+++ db_gateway/app.py
@@ -1,6 +1,7 @@
 import os
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, json
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
@@ -13,6 +14,27 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{}:{}@{}/{}'.format(
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(120), nullable=False)
+
+    @property
+    def password(self):
+        return self.password
+    
+    @password.setter
+    def password(self, text_password):
+        self.password_hash = bcrypt.generate_password_hash(text_password).decode('utf-8')
+
+    def check_password_correction(self, attempted_password):
+        return bcrypt.check_password_hash(self.password_hash, attempted_password)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
 
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -20,7 +42,6 @@ class Student(db.Model):
     lastname = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(80), unique=True, nullable=False)
     age = db.Column(db.Integer)
-    bio = db.Column(db.Text)
 
     def __repr__(self):
         return f'<Student {self.firstname}>'
@@ -31,7 +52,52 @@ with app.app_context():
 
 @app.route('/api/hello', methods=['GET'])
 def get_hello():
-    return jsonify({'message': 'Hello from the server!'})
+    return jsonify({'message': 'Hello from the db_gateway server!'})
 
+@app.route('/api/register', methods=['POST'])
+def register_user():          
+    try:
+        print("Call register api!")
+        user = json.loads(request.json)
+
+        new_username = user["username"]
+        new_password = user["password"]
+        new_email = user["email"]
+
+        if new_username and new_password and new_email and request.method == 'POST':
+            new_user = User(username=new_username, 
+                                password=new_password,
+                                email=new_email)
+            
+            db.session.add(new_user)
+            db.session.commit()
+
+            response = jsonify('User added successfully!')
+            response.status_code = 200
+            return response
+        else:
+            response = jsonify('No data available')
+            response.status_code = 204
+            return response
+    except Exception as e:
+        print(e)
+        response = "Internal server error!"
+        response.status_code = 500
+        return response
+    
+@app.route('/api/login', methods=['GET'])
+def login_user(): 
+    auth = request.authorization
+
+    if not auth:
+        return "missing credentials", 401
+    
+    attempted_user = User.query.filter_by(email=auth.username).first()
+    
+    if attempted_user and attempted_user.check_password_correction(attempted_password=auth.password):
+        return "Successfuly logged", 200
+    else:
+        return "invalid credentials", 401
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
---
+++ server/web/routes.py
@@ -0,0 +1,79 @@
+from web import app
+from flask import render_template, redirect, url_for, jsonify, flash
+from web.forms import RegisterForm, LoginForm
+from web.db_api import register_user_api, login_user_api
+
+@app.route('/api/hello', methods=['GET'])
+def get_hello():
+    return jsonify({'message': 'Hello from the server!'})
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+
+        response = login_user_api(form.email.data, form.password.data)
+
+        if response and response.status_code == 200:
+            flash('Success! You logged in!', category='success')
+            return redirect(url_for('home_page'))
+        else:
+            flash('Username or password are not correct! Please try again!', category='danger')
+        # attempted_user = User.query.filter_by(email=form.email.data).first()
+
+        # if attempted_user and attempted_user.check_password_correction(attempted_password=form.password.data):
+        #     login_user(attempted_user)
+        #     flash('Success! You logged in!', category='success')
+        #     return redirect(url_for('home_page'))
+        # else:
+        #     flash('Username or password are not correct! Please try again!', category='danger')
+
+    return render_template('login.html', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register_page():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        new_user = {'username': form.username.data, 'password': form.password1.data, 'email': form.email.data}
+
+        response = register_user_api(new_user)
+        
+        if response:
+            flash(f'Success! User {new_user["username"]} created succesfully!', category='success')
+            return render_template('home.html')
+        else:
+            return jsonify({'message': 'No valid response'}) 
+    
+    if form.errors != {}:
+        for err_msj in form.errors.values():
+            flash(f'Error when creating an user:{err_msj}', category='danger')
+
+    return render_template('register.html', form=form)
+
+@app.route('/api/test', methods=['GET'])
+def test():
+    #response = requests.get('http://db_gateway:5001/api/hello')
+
+    # new_user = {"username": "newuser", "password": "pass", "email": "emailtest"}
+
+    # headers={
+    #     'Content-type':'application/json', 
+    #     'Accept':'application/json'
+    # }
+
+    #response = requests.post('http://db_gateway:5001/api/register', headers=headers, json=json.dumps(new_user))
+
+    response = login_user_api("cristi233@yahoo.com", "passwordabc")
+
+    if response and response.status_code == 200:
+        print("Got here!")
+        return response.content, response.status_code
+    else:
+        return jsonify({'message': 'No valid response'})
